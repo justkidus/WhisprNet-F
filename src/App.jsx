95c3939c7ed4a7cd6dd930a33691f070d10f0325
justkiduss <kidusteshome036@gmail.com>
2025-06-04 00:41:26 +0300

    fixing build errors

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Setting from './pages/setting.page';
 // import SidebarDemo from './pages/dash.board';
 // import Setting from './pages/setting.page';
 import { useChatstore } from './store/useChatstore';
+import LogoutPage from './pages/logout';
 import Page from './dashboard/page';
 function App() {
 	const { selectedUser } = useChatstore();
