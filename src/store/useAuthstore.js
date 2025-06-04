e041e2d02b9c9022be9b4505cea10d1cf12b3907
justkiduss <kidusteshome036@gmail.com>
2025-06-04 22:52:09 +0300

    fixing hostigng error

--- src/store/useAuthstore.js
+++ src/store/useAuthstore.js
@@ -2,7 +2,8 @@ import { create } from 'zustand';
 import axiosInstance from '@/lib/axios';
 import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
-const BASE_URL = 'http://localhost:8001';
+// const BASE_URL = 'http://localhost:8001';
+const BASE_URL = 'https://whisprnet-b.onrender.com/api';
 export const useAuthStore = create((set, get) => ({
 	authUser: null,
 	isSigningUp: false,
@@ -79,19 +80,20 @@ export const useAuthStore = create((set, get) => ({
 	connectSocket: () => {
 		const { authUser } = get();
 		if (!authUser || get().socket?.connected) return;
-		const socket = io(BASE_URL,{
+		const socket = io(BASE_URL, {
 			query: {
 				userId: authUser._id,
-			},});
+			},
+		});
 		socket.connect();
 
 		set({ socket: socket });
 
 		socket.on('getOnlineUsers', (userIds) => {
 			set({ onlineUsers: userIds });
-		     });
+		});
 	},
 	disconnectSocket: () => {
 		if (get().socket?.connected) get().socket.disconnect();
-	}
+	},
 }));
