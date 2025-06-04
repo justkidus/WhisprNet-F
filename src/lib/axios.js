41d69200fc11b081bfae287426aa330892874e52
justkiduss <kidusteshome036@gmail.com>
2025-06-04 07:39:07 +0300

    deploying

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:8001/api',
+	// baseURL: 'http://localhost:8001/api',
+	baseURL: 'https://whispr-net-b.vercel.app/api',
 	withCredentials: true,
 });
 export default axiosInstance;
